dacd66bcc28c5d9ef5f2b72f13994529a6b8377f
jeffwang0516 <jeff1jeffo@gmail.com>
2019-09-27 21:02:06 +0800

    Update cleaning commands

--- run.sh
+++ run.sh
@@ -4,6 +4,12 @@ pyinstaller --onefile "main.py"
 echo ""
 echo "Executable ./main will be generated to dist/"
 echo ""
+
 echo "Removing intermediate files..."
+
 rm -fr build/ __pycache__/
-rm -fv *.spec app/*.c app/*.so
+find . -name "*.spec" -type f -delete
+find . -name "*.so" -type f -delete
+find . -name "*.c" -type f -delete
+
+echo "Done."
